36faef89120a95397445a657e7febae8e8c9408b
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-08-02 09:06:27 +0530

    Integrating Circle Ci

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Abhishek jain Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
+        tenetur aperiam totam provident consequatur atque corrupti eos illo
+        dolorum. Consequatur, qui? Fugit dolor ex labore, magni explicabo
+        impedit facere soluta!
+      </p>
     </div>
   );
 }
